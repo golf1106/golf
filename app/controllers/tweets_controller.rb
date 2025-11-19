6913c3ef01c7e74cf131449b9aabce5d23fe685c
golf1106 <m.yuta.1106@icloud.com>
2025-11-19 19:06:32 +0900

    （変更内容の説明）

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -56,10 +56,19 @@ class TweetsController < ApplicationController
   def edit
     @tweet = Tweet.find(params[:id])
   end
+  
+ def update
+    tweet = Tweet.find(params[:id])
+    if tweet.update(tweet_params)
+      redirect_to :action => "show", :id => tweet.id
+    else
+      redirect_to :action => "new"
+    end
+  end
 
   def destroy
-    @tweet = Tweet.find(params[:id])
-    @tweet.destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
     redirect_to action: :index
   end
 
